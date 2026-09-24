e221edfa640f62d5dfe39380115a90d09cf5e351
yujaekyung <rew623@gmail.com>
2026-09-25 00:35:15 +0900

    Update Firebase configuration with new credentials

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // ★ Firebase 콘솔 > 프로젝트 설정 > 내 앱(웹)에 나오는 값을 그대로 붙여 넣으세요
 export const FIREBASE_CONFIG = {
-  apiKey: "여기에",
-  authDomain: "여기에.firebaseapp.com",
-  projectId: "여기에",
-  storageBucket: "여기에.firebasestorage.app",
-  messagingSenderId: "여기에",
-  appId: "여기에"
+  apiKey: "AIzaSyDgBMFNCskNruMKsRjdIDCP_jZ0-mD1Hlo",
+  authDomain: "soeun-diary.firebaseapp.com",
+  projectId: "soeun-diary",
+  storageBucket: "soeun-diary.firebasestorage.app",
+  messagingSenderId: "480313932957",
+  appId: "1:480313932957:web:4add9e5717284058c46874"
 };
